7ed81a32efdae40d9a29e77e29c5565c1b60f539
sarredondq <regeaxd@gmail.com>
2026-01-31 22:18:19 -0500

    Update HTTP handler to return current time and hostname

--- k8s-hans-on/backend/src/main.go
+++ k8s-hans-on/backend/src/main.go
@@ -2,18 +2,22 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
 func main() {
-	http.HandleFunc("/", handleHello)
+	http.HandleFunc("/", serverHTTP)
 
 	log.Println("Server starting on :9090")
 	log.Fatal(http.ListenAndServe(":9090", nil))
 }
 
-func handleHello(w http.ResponseWriter, r *http.Request) {
+func serverHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(map[string]string{"message": "Hello, World!"})
+	resp := fmt.Sprintf("La hora es %s y el hostname es %s", time.Now(), os.Getenv("HOSTNAME"))
+	json.NewEncoder(w).Encode(map[string]string{"message": resp})
 }
